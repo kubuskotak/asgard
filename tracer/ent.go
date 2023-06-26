4420d96dce6d6766e293d9edaf940edde8dbe4b1
suryakencana007 <nanang.jobs@gmail.com>
2023-06-26 14:46:32 +0700

    feat: hooks ent tracing query

--- tracer/ent.go
+++ tracer/ent.go
@@ -2,12 +2,16 @@ package tracer
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"reflect"
 	"strings"
 	"time"
 
 	"entgo.io/ent"
+	"entgo.io/ent/dialect"
+	"github.com/google/uuid"
+	"github.com/rs/zerolog"
 )
 
 // EntHook defines the "mutation middleware". A function that gets a Mutator
@@ -66,4 +70,187 @@ func EntInterceptor() ent.Interceptor {
 			return v, err
 		})
 	})
+}
+
+// EntDriver is a driver that logs all driver operations.
+type EntDriver struct {
+	dialect.Driver                                              // underlying driver.
+	log            func(context.Context, zerolog.Level, ...any) // log function. defaults to log.Println.
+}
+
+// EntDriverWithContext gets a driver and a logging function, and returns
+// a new tracer-driver that prints all outgoing operations with context.
+func EntDriverWithContext(d dialect.Driver, logger func(context.Context, zerolog.Level, ...any)) dialect.Driver {
+	drv := &EntDriver{d, logger}
+	return drv
+}
+
+// Exec logs its params and calls the underlying driver Exec method.
+func (d *EntDriver) Exec(ctx context.Context, query string, args, v any) error {
+	if err := d.Driver.Exec(ctx, query, args, v); err != nil {
+		d.log(ctx, zerolog.ErrorLevel, fmt.Sprintf("driver.Exec: query=%v args=%v", query, args), err)
+		return err
+	}
+	d.log(ctx, zerolog.InfoLevel, fmt.Sprintf("driver.Exec: query=%v args=%v", query, args), nil)
+	return nil
+}
+
+// ExecContext logs its params and calls the underlying driver ExecContext method if it is supported.
+func (d *EntDriver) ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error) {
+	drv, ok := d.Driver.(interface {
+		ExecContext(context.Context, string, ...any) (sql.Result, error)
+	})
+	if !ok {
+		var err = fmt.Errorf("Driver.ExecContext is not supported")
+		d.log(ctx, zerolog.ErrorLevel, fmt.Sprintf("driver.ExecContext: query=%v args=%v", query, args), err)
+		return nil, err
+	}
+	result, err := drv.ExecContext(ctx, query, args...)
+	if err != nil {
+		d.log(ctx, zerolog.ErrorLevel, fmt.Sprintf("driver.ExecContext: query=%v args=%v", query, args), err)
+		return nil, err
+	}
+	d.log(ctx, zerolog.InfoLevel, fmt.Sprintf("driver.ExecContext: query=%v args=%v", query, args), nil)
+	return result, nil
+}
+
+// Query logs its params and calls the underlying driver Query method.
+func (d *EntDriver) Query(ctx context.Context, query string, args, v any) error {
+	if err := d.Driver.Query(ctx, query, args, v); err != nil {
+		d.log(ctx, zerolog.ErrorLevel, fmt.Sprintf("driver.Query: query=%v args=%v", query, args), err)
+		return err
+	}
+	d.log(ctx, zerolog.InfoLevel, fmt.Sprintf("driver.Query: query=%v args=%v", query, args))
+	return nil
+}
+
+// QueryContext logs its params and calls the underlying driver QueryContext method if it is supported.
+func (d *EntDriver) QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error) {
+	drv, ok := d.Driver.(interface {
+		QueryContext(context.Context, string, ...any) (*sql.Rows, error)
+	})
+	if !ok {
+		var err = fmt.Errorf("Driver.QueryContext is not supported")
+		d.log(ctx, zerolog.ErrorLevel, fmt.Sprintf("driver.QueryContext: query=%v args=%v", query, args), err)
+		return nil, err
+	}
+	rows, err := drv.QueryContext(ctx, query, args...)
+	if err != nil {
+		d.log(ctx, zerolog.ErrorLevel, fmt.Sprintf("driver.QueryContext: query=%v args=%v", query, args), err)
+		return nil, err
+	}
+	d.log(ctx, zerolog.InfoLevel, fmt.Sprintf("driver.QueryContext: query=%v args=%v", query, args))
+	return rows, nil
+}
+
+// Tx adds a log-id for the transaction and calls the underlying driver Tx command.
+func (d *EntDriver) Tx(ctx context.Context) (dialect.Tx, error) {
+	tx, err := d.Driver.Tx(ctx)
+	if err != nil {
+		return nil, err
+	}
+	id := uuid.New().String()
+	d.log(ctx, zerolog.InfoLevel, fmt.Sprintf("driver.Tx(%s): started", id))
+	return &Tx{tx, id, d.log, ctx}, nil
+}
+
+// BeginTx adds a log-id for the transaction and calls the underlying driver BeginTx command if it is supported.
+func (d *EntDriver) BeginTx(ctx context.Context, opts *sql.TxOptions) (dialect.Tx, error) {
+	drv, ok := d.Driver.(interface {
+		BeginTx(context.Context, *sql.TxOptions) (dialect.Tx, error)
+	})
+	if !ok {
+		var err = fmt.Errorf("Driver.BeginTx is not supported")
+		d.log(ctx, zerolog.ErrorLevel, "driver.BeginTx: cannot started", err)
+		return nil, err
+	}
+	tx, err := drv.BeginTx(ctx, opts)
+	if err != nil {
+		d.log(ctx, zerolog.InfoLevel, "driver.BeginTx: cannot started", err)
+		return nil, err
+	}
+	id := uuid.New().String()
+	d.log(ctx, zerolog.InfoLevel, fmt.Sprintf("driver.BeginTx(%s): started", id))
+	return &Tx{tx, id, d.log, ctx}, nil
+}
+
+// Tx is a transaction implementation that logs all transaction operations.
+type Tx struct {
+	dialect.Tx                                              // underlying transaction.
+	id         string                                       // transaction logging id.
+	log        func(context.Context, zerolog.Level, ...any) // log function. defaults to fmt.Println.
+	ctx        context.Context                              // underlying transaction context.
+}
+
+// Exec logs its params and calls the underlying transaction Exec method.
+func (d *Tx) Exec(ctx context.Context, query string, args, v any) error {
+	if err := d.Tx.Exec(ctx, query, args, v); err != nil {
+		d.log(ctx, zerolog.ErrorLevel, fmt.Sprintf("Tx(%s).Exec: query=%v args=%v", d.id, query, args), err)
+		return err
+	}
+	d.log(ctx, zerolog.InfoLevel, fmt.Sprintf("Tx(%s).Exec: query=%v args=%v", d.id, query, args))
+	return nil
+}
+
+// ExecContext logs its params and calls the underlying transaction ExecContext method if it is supported.
+func (d *Tx) ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error) {
+	drv, ok := d.Tx.(interface {
+		ExecContext(context.Context, string, ...any) (sql.Result, error)
+	})
+	if !ok {
+		var err = fmt.Errorf("Tx.ExecContext is not supported")
+		d.log(ctx, zerolog.ErrorLevel, fmt.Sprintf("Tx(%s).Exec: query=%v args=%v", d.id, query, args), err)
+		return nil, err
+	}
+	d.log(ctx, zerolog.InfoLevel, fmt.Sprintf("Tx(%s).ExecContext: query=%v args=%v", d.id, query, args))
+	return drv.ExecContext(ctx, query, args...)
+}
+
+// Query logs its params and calls the underlying transaction Query method.
+func (d *Tx) Query(ctx context.Context, query string, args, v any) error {
+	if err := d.Tx.Query(ctx, query, args, v); err != nil {
+		d.log(ctx, zerolog.ErrorLevel, fmt.Sprintf("Tx(%s).Exec: query=%v args=%v", d.id, query, args), err)
+		return err
+	}
+	d.log(ctx, zerolog.InfoLevel, fmt.Sprintf("Tx(%s).Query: query=%v args=%v", d.id, query, args))
+	return nil
+}
+
+// QueryContext logs its params and calls the underlying transaction QueryContext method if it is supported.
+func (d *Tx) QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error) {
+	drv, ok := d.Tx.(interface {
+		QueryContext(context.Context, string, ...any) (*sql.Rows, error)
+	})
+	if !ok {
+		var err = fmt.Errorf("Tx.QueryContext is not supported")
+		d.log(ctx, zerolog.ErrorLevel, fmt.Sprintf("Tx(%s).Exec: query=%v args=%v", d.id, query, args), err)
+		return nil, err
+	}
+	row, err := drv.QueryContext(ctx, query, args...)
+	if err != nil {
+		d.log(ctx, zerolog.ErrorLevel, fmt.Sprintf("Tx(%s).Exec: query=%v args=%v", d.id, query, args), err)
+		return nil, err
+	}
+	d.log(ctx, zerolog.InfoLevel, fmt.Sprintf("Tx(%s).QueryContext: query=%v args=%v", d.id, query, args))
+	return row, nil
+}
+
+// Commit logs this step and calls the underlying transaction Commit method.
+func (d *Tx) Commit() error {
+	if err := d.Tx.Commit(); err != nil {
+		d.log(d.ctx, zerolog.ErrorLevel, fmt.Sprintf("Tx(%s): committed", d.id), err)
+		return err
+	}
+	d.log(d.ctx, zerolog.InfoLevel, fmt.Sprintf("Tx(%s): committed", d.id))
+	return nil
+}
+
+// Rollback logs this step and calls the underlying transaction Rollback method.
+func (d *Tx) Rollback() error {
+	if err := d.Tx.Rollback(); err != nil {
+		d.log(d.ctx, zerolog.ErrorLevel, fmt.Sprintf("Tx(%s): rollbacked", d.id), err)
+		return err
+	}
+	d.log(d.ctx, zerolog.InfoLevel, fmt.Sprintf("Tx(%s): rollbacked", d.id))
+	return nil
 }
